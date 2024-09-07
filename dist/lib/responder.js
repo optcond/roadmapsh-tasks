86d29544f5ce5b818e50a58aff3f3c3c1453e5df
optcond <64412497+optcond@users.noreply.github.com>
2024-09-07 19:13:42 +0300

    Delete dist directory

--- dist/lib/responder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Responder = void 0;
-class Responder {
-    output(data) {
-        data.forEach((d) => console.log(d));
-    }
-}
-exports.Responder = Responder;
